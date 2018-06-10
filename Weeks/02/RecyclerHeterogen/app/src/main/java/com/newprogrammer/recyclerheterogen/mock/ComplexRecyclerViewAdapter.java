7d392be921b5a300946031c150c381c195d5a0aa
Acer\Boss2 <novopashinwm@mail.ru>
2018-06-10 06:43:28 +0300

    +1

---
+++ Weeks/02/RecyclerHeterogen/app/src/main/java/com/newprogrammer/recyclerheterogen/mock/ComplexRecyclerViewAdapter.java
@@ -0,0 +1,95 @@
+package com.newprogrammer.recyclerheterogen.mock;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.newprogrammer.recyclerheterogen.R;
+
+import java.util.List;
+
+public class ComplexRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    // The items to display in your RecyclerView
+    private List<Object> items;
+
+    private final int USER = 0, IMAGE = 1;
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public ComplexRecyclerViewAdapter(List<Object> items) {
+        this.items = items;
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return this.items.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (items.get(position) instanceof User) {
+            return USER;
+        } else if (items.get(position) instanceof String) {
+            return IMAGE;
+        }
+        return -1;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        RecyclerView.ViewHolder viewHolder;
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+
+        switch (viewType) {
+            case USER:
+                View v1 = inflater.inflate(R.layout.layout_viewholder1, viewGroup, false);
+                viewHolder = new ViewHolder1(v1);
+                break;
+            case IMAGE:
+                View v2 = inflater.inflate(R.layout.layout_viewholder2, viewGroup, false);
+                viewHolder = new ViewHolder2(v2);
+                break;
+            default:
+                View v = inflater.inflate(android.R.layout.simple_list_item_1, viewGroup, false);
+                viewHolder = new RecyclerViewSimpleTextViewHolder(v);
+                break;
+        }
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+        switch (viewHolder.getItemViewType()) {
+            case USER:
+                ViewHolder1 vh1 = (ViewHolder1) viewHolder;
+                configureViewHolder1(vh1, position);
+                break;
+            case IMAGE:
+                ViewHolder2 vh2 = (ViewHolder2) viewHolder;
+                configureViewHolder2(vh2, position);
+                break;
+            default:
+                RecyclerViewSimpleTextViewHolder vh = (RecyclerViewSimpleTextViewHolder) viewHolder;
+                configureDefaultViewHolder(vh, position);
+                break;
+        }
+    }
+
+    private void configureDefaultViewHolder(RecyclerViewSimpleTextViewHolder vh, int position) {
+        vh.getLabel().setText((CharSequence) items.get(position));
+    }
+
+    private void configureViewHolder1(ViewHolder1 vh1, int position) {
+        User user = (User) items.get(position);
+        if (user != null) {
+            vh1.getLabel1().setText("Name: " + user.name);
+            vh1.getLabel2().setText("Hometown: " + user.hometown);
+        }
+    }
+
+    private void configureViewHolder2(ViewHolder2 vh2) {
+        vh2.getImageView().setImageResource(R.drawable.sample_golden_gate);
+    }
+}
---
+++ Weeks/02/RecyclerHeterogen/app/src/main/java/com/newprogrammer/recyclerheterogen/mock/User.java
@@ -0,0 +1,7 @@
+package com.newprogrammer.recyclerheterogen.mock;
+
+class User {
+
+    public String name;
+    public String hometown;
+}

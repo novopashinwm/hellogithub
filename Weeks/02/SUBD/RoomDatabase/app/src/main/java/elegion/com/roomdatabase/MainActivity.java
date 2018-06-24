e3e317982fa5bdfc9b5a6b2c20cb3ba46886b795
Acer\Boss2 <novopashinwm@mail.ru>
2018-06-24 19:05:50 +0300

    Add Room base

---
+++ Weeks/02/SUBD/RoomDatabase/app/src/main/java/elegion/com/roomdatabase/MainActivity.java
@@ -0,0 +1,66 @@
+package elegion.com.roomdatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import elegion.com.roomdatabase.database.Album;
+import elegion.com.roomdatabase.database.MusicDao;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mAddBtn;
+    private Button mGetBtn;
+
+    // добавить базу данных Room ----
+    // вставить данные / извлечь данные ---
+    // добавить контент провайдер над Room ---
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final MusicDao musicDao = ((AppDelegate) getApplicationContext()).getMusicDatabase().getMusicDao();
+
+        mAddBtn = (findViewById(R.id.add));
+        mAddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                musicDao.insertAlbums(createAlbums());
+            }
+        });
+
+        mGetBtn = findViewById(R.id.get);
+        mGetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast(musicDao.getAlbums());
+            }
+        });
+
+    }
+
+    private List<Album> createAlbums() {
+        List<Album> albums = new ArrayList<>(10);
+        for (int i = 0; i < 30; i++) {
+            albums.add(new Album(i, "album " + i, "release" + System.currentTimeMillis()));
+        }
+
+        return albums;
+    }
+
+    private void showToast(List<Album> albums) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0, size = albums.size(); i < size; i++) {
+            builder.append(albums.get(i).toString()).append("\n");
+        }
+
+        Toast.makeText(this, builder.toString(), Toast.LENGTH_SHORT).show();
+
+    }
+}
